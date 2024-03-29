0951e3d18b1d1280f6c3fb76b54b87a0ee75b24f
xinghunm <316032603@qq.com>
2024-03-29 15:14:07 +0800

    init atomos-3d repository

---
+++ packages/atomos-3d/src/examples/app.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+export const App = () => {
+  return <Container>hello</Container>;
+};
