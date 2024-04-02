e326874474f576e1a8af50e07796dce21cb7f811
xinghunm <316032603@qq.com>
2024-04-02 15:19:46 +0800

    add readFile function to support multiple read types

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,4 +1,4 @@
 export * from './delay';
 export * from './save-as-file';
-export * from './read-file-as-string';
+export * from './read-file';
 export * from './format-bytes-as-readable-size';
---
+++ packages/utils/src/read-file.ts
@@ -0,0 +1,36 @@
+export enum ReadAsType {
+  'Text',
+  'BinaryString',
+  'DataURL',
+  'ArrayBuffer',
+}
+
+export const readFile = (file: File, readAs: ReadAsType): Promise<string | ArrayBuffer> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      resolve(event.target?.result);
+    };
+
+    reader.onerror = (event) => {
+      reject(event.target?.error);
+    };
+
+    switch (readAs) {
+      case ReadAsType.Text:
+        reader.readAsText(file);
+        break;
+      case ReadAsType.BinaryString:
+        reader.readAsBinaryString(file);
+        break;
+      case ReadAsType.DataURL:
+        reader.readAsDataURL(file);
+        break;
+      case ReadAsType.ArrayBuffer:
+        reader.readAsArrayBuffer(file);
+        break;
+      default:
+        reject(new Error('Unsupported read type'));
+    }
+  });
