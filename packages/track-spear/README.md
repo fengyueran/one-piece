7c93bc02e7b8a3a86fc599524883058b3871886d
xinghunm <316032603@qq.com>
2024-04-03 14:15:17 +0800

    add track-spear

---
+++ packages/track-spear/README.md
@@ -0,0 +1,3 @@
+# @xinghunm/track-spear
+
+@xinghunm/track-spear 是原生 JS 实现的埋点库。
---
+++ packages/track-spear/examples/index.ts
@@ -0,0 +1,6 @@
+import * as lib from '../src';
+
+const test = () => {
+  console.log('add:', lib.add(1, 2));
+};
+test();
