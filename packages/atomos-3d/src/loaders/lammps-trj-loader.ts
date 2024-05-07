760752ad0025631ccd18b8dc2387db0a85266a59
xinghunm <316032603@qq.com>
2024-05-07 13:30:17 +0800

    support to parse lammps trj file by reader

--- packages/atomos-3d/src/loaders/lammps-trj-loader.ts
+++ packages/atomos-3d/src/loaders/lammps-trj-loader.ts
@@ -13,11 +13,29 @@ interface AtomInfo {
   z: number;
 }
 
+const parseLammpsData = (frameStr: string) => {
+  const parts = frameStr.split('TEM: ATOMS id element x y z');
+
+  const lines = parts[1].split('\n').slice(1, -1);
+  const atoms = lines.map((line) => {
+    const lineParts = line.split(' ');
+    return {
+      id: parseInt(lineParts[0]),
+      element: lineParts[1],
+      x: parseFloat(lineParts[2]),
+      y: parseFloat(lineParts[3]),
+      z: parseFloat(lineParts[4]),
+    };
+  });
+
+  console.log('atoms', atoms.length);
+
+  return atoms;
+};
+
 export class LammpsTrjLoader {
   private _paused = false;
-  private _preStr = '';
-  private _models: AtomInfo[][] = [];
-  private _streamController?: ReadableStreamDefaultController;
+  private _buffer = ''; //缓冲区
   private _resumePromiseResolve?: (value: unknown) => void;
   private _onModel: (atomInfos: AtomInfo[]) => void;
 
@@ -26,137 +44,48 @@ export class LammpsTrjLoader {
     this._onModel = onModel;
   }
 
-  parseLammpsData = (data: string, preStr = '') => {
-    const frames = `${preStr}\n${data}`.split('ITEM: TIMESTEP');
-    this._preStr = '';
-    const validFrames = [];
-
-    frames.slice(1).forEach((frame, index) => {
-      const parts = frame.split('TEM: ATOMS id element x y z');
-      // if (!parts[1]) {
-      //   debugger; //eslint-disable-line
-      //   return [];
-      // }
-      const lines = parts[1].split('\n').slice(1, -1);
-
-      if (lines.length < 5666) {
-        this._preStr = frame;
-      } else {
-        const values = lines.map((line) => {
-          const lineParts = line.split(' ');
-          return {
-            id: parseInt(lineParts[0]),
-            element: lineParts[1],
-            x: parseFloat(lineParts[2]),
-            y: parseFloat(lineParts[3]),
-            z: parseFloat(lineParts[4]),
-          };
-        });
-
-        console.log('values', values.length);
-
-        validFrames.push(values);
-      }
-    });
-    return validFrames;
-  };
-
-  fetchAndStream = async (url) => {
-    const response = await fetch(url);
-    const reader = response.body.getReader();
-    const decoder = new TextDecoder('utf-8');
-    let receivedLength = 0;
-    const that = this;
-
-    const handleChunk = (value: Uint8Array) => {
-      this.pause();
-      const textChunk = decoder.decode(value, { stream: true });
-      const frameDatas = this.parseLammpsData(textChunk);
-
-      frameDatas.forEach((frameData) => {
-        this._onModel(frameData);
-      });
-    };
-
-    // 创建一个新的 ReadableStream 来处理数据流
-    const stream = new ReadableStream({
-      start(controller) {
-        console.log('Stream started');
-      },
-      async pull(controller) {
-        while (true) {
-          if (that._paused) {
-            // 等待恢复信号
-            await new Promise(
-              (resolve) => (that._resumePromiseResolve = resolve)
-            );
-          }
-
-          const { done, value } = await reader.read();
-          handleChunk(value);
-          if (done) {
-            controller.close();
-            console.log('Stream completed');
-            break; // 退出循环，防止无限循环
-          }
-          // console.log('receivedLength', receivedLength);
-
-          receivedLength += value.length; // 更新接收到的数据长度
-          controller.enqueue(value); // 将数据块放入流中
-          // await delay();
-        }
-      },
-      cancel() {
-        console.log('Stream cancelled');
-      },
-    });
-
-    // 使用这个流的示例（根据具体情况）
-    const newResponse = new Response(stream);
-  };
-  // fetchAndStream = async (url: string) => {
+  // fetchAndStream = async (url) => {
   //   const response = await fetch(url);
   //   const reader = response.body.getReader();
-  //   // const decoder = new TextDecoder('utf-8');
-
+  //   const decoder = new TextDecoder('utf-8');
   //   let receivedLength = 0;
-  //   // const that = this;
+  //   const that = this;
 
-  //   // const handleChunk = (value: Uint8Array) => {
-  //   //   this.pause();
-  //   //   const textChunk = decoder.decode(value, { stream: true });
-  //   //   const frameDatas = parseLammpsData(textChunk);
+  //   const handleChunk = (value: Uint8Array) => {
+  //     this.pause();
+  //     const textChunk = decoder.decode(value, { stream: true });
+  //     const frameDatas = this.parseLammpsData(textChunk);
 
-  //   //   frameDatas.forEach((frameData) => {
-  //   //     this._onModel(frameData);
-  //   //   });
-  //   // };
+  //     frameDatas.forEach((frameData) => {
+  //       this._onModel(frameData);
+  //     });
+  //   };
 
-  //   // 创建一个新的 ReadableStream 来处理暂停和恢复
+  //   // 创建一个新的 ReadableStream 来处理数据流
   //   const stream = new ReadableStream({
   //     start(controller) {
-  //       // that._streamController = controller;
+  //       console.log('Stream started');
   //     },
   //     async pull(controller) {
   //       while (true) {
-  //         // if (that._paused) {
-  //         //   // 等待恢复信号
-  //         //   await new Promise(
-  //         //     (resolve) => (that._resumePromiseResolve = resolve)
-  //         //   );
-  //         // }
+  //         if (that._paused) {
+  //           // 等待恢复信号
+  //           await new Promise(
+  //             (resolve) => (that._resumePromiseResolve = resolve)
+  //           );
+  //         }
 
   //         const { done, value } = await reader.read();
+  //         handleChunk(value);
   //         if (done) {
   //           controller.close();
   //           console.log('Stream completed');
-  //           return;
+  //           break; // 退出循环，防止无限循环
   //         }
-  //         console.log('receivedLength', receivedLength);
-  //         // handleChunk(value);
-  //         // receivedLength += value.length;
+  //         // console.log('receivedLength', receivedLength);
 
-  //         controller.enqueue(value);
+  //         receivedLength += value.length; // 更新接收到的数据长度
+  //         controller.enqueue(value); // 将数据块放入流中
   //         // await delay();
   //       }
   //     },
@@ -165,51 +94,58 @@ export class LammpsTrjLoader {
   //     },
   //   });
 
-  //   return new Response(stream).text();
+  //   // 使用这个流的示例（根据具体情况）
+  //   const newResponse = new Response(stream);
   // };
 
-  read = () => {
-    this._paused = false;
+  fetchAndStream = async (url: string) => {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    // 获取 ReadableStream
+    const reader = response.body?.getReader();
+
+    if (!reader) {
+      throw new Error('reader was not ok');
+    }
+
+    // eslint-disable-next-line
+    while (true) {
+      if (this._paused) {
+        await new Promise((resolve) => (this._resumePromiseResolve = resolve));
+      }
+      const { done, value } = await reader.read();
+
+      this._buffer += new TextDecoder().decode(value, { stream: true });
+      this.processBuffer();
+      if (done) {
+        console.log('Stream completed');
+        break;
+      }
+    }
   };
 
-  // fetchAndStream = async (url) => {
-  //   const handleChunk = (value: Uint8Array) => {
-  //     // this.pause();
-  //     const textDecoder = new TextDecoder();
-  //     const textChunk = textDecoder.decode(value, { stream: true });
-  //     const frameDatas = parseLammpsData(textChunk);
-  //     // this._models = [...this._models, ...frameDatas];
-  //     frameDatas.forEach((frameData) => {
-  //       this._onModel(frameData);
-  //     });
-  //   };
+  processBuffer = () => {
+    this.pause();
+    let start = this._buffer.indexOf('ITEM: TIMESTEP');
 
-  //   const response = await fetch(url);
+    while (start !== -1) {
+      const end = this._buffer.indexOf('ITEM: TIMESTEP', start + 1);
+      if (end === -1) break;
 
-  //   // 确认响应有效
-  //   if (!response.ok) {
-  //     throw new Error('Network response was not ok');
-  //   }
+      // 处理完整的帧
+      const frameStr = this._buffer.substring(start, end);
+      const frameAtoms = parseLammpsData(frameStr);
+      this._onModel(frameAtoms);
 
-  //   // 获取 ReadableStream
-  //   const reader = response.body.getReader();
-  //   let receivedLength = 0; // 接收到的数据量
-
-  //   // 逐步读取数据
-  //   while (true) {
-  //     await delay();
-  //     const { done, value } = await reader.read();
-
-  //     if (done) {
-  //       console.log('数据接收完毕');
-  //       break;
-  //     }
-
-  //     receivedLength += value.length;
-  //     console.log('value.length', value.length);
-  //     handleChunk(value);
-  //   }
-  // };
+      // 更新缓冲区
+      this._buffer = this._buffer.substring(end);
+      start = this._buffer.indexOf('ITEM: TIMESTEP');
+    }
+  };
 
   pause = () => {
     this._paused = true;
