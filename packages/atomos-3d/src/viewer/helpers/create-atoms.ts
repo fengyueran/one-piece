7263a70d1d6f6263f52cc10f19993e5a2b6389c1
xinghunm <316032603@qq.com>
2024-08-14 11:13:29 +0800

    fix bone position

--- packages/atomos-3d/src/viewer/helpers/create-atoms.ts
+++ packages/atomos-3d/src/viewer/helpers/create-atoms.ts
@@ -210,8 +210,8 @@ export const createAtomsFromPdb = (pdbText: string) => {
   geometryAtoms.boundingBox?.getCenter(offset).negate();
 
   //将原子的几何结构沿着计算出的偏移量平移，使得模型的几何中心与坐标系的原点对齐
-  // geometryAtoms.translate(offset.x, offset.y, offset.z);
-  // geometryBonds.translate(offset.x, offset.y, offset.z);
+  geometryAtoms.translate(offset.x, offset.y, offset.z);
+  geometryBonds.translate(offset.x, offset.y, offset.z);
 
   const positions = geometryAtoms.getAttribute('position');
 
@@ -233,7 +233,16 @@ export const createAtomsFromPdb = (pdbText: string) => {
 
   const lines = pdb.json.bondAtomTypes.length
     ? createLines(geometryBonds, pdb.json.bondAtomTypes)
-    : createLinesByAtomDistance(pdb.json.atoms);
+    : createLinesByAtomDistance(
+        pdb.json.atoms.map((atom) => {
+          return [
+            atom[0] + offset.x,
+            atom[1] + offset.y,
+            atom[2] + offset.z,
+            atom[3],
+          ];
+        })
+      );
 
   return { atoms, lines };
 };
